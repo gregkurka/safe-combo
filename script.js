75ae9367a21c6b046a9831254a7a05adab9807eb
Greg Kurka <gregkurka@gmail.com>
2024-12-11 13:56:25 -0600

    added full project

---
+++ script.js
@@ -0,0 +1,19 @@
+//creates a string and calculates combo numbers with arithmatic
+const msg =
+  "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+
+const combo1 = 100 / 10;
+const combo2 = 20 * 2;
+const combo3 = 59 - 20;
+
+/*draws attention to my psuedocode but I will also copy it to this comment:
+_PseudoCode_
+
+1. Create the combination 10-40-39 via arithmetic operations
+2. Display "You have received this message because you have been chosen to open an important vault. Here is the secret combination:"
+3. Show the combination "10-40-39" to the user
+*/
+alert("Check out my pseudocode in my readme or on the html page.");
+
+//alerts with a string literal
+alert(`${msg} ${combo1} - ${combo2} - ${combo3}`);
